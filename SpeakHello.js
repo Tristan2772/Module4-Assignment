e3f5a39dcb5b0716f3c99aff1bd86ea48ba771e2
Tristan2772 <108966760+Tristan2772@users.noreply.github.com>
2024-11-27 23:02:43 -0600

    Update SpeakHello.js to include goodbye.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -35,3 +35,26 @@
     window.helloSpeaker = helloSpeaker;
 
 }) (window);
+
+(function (window) {
+
+    let byeSpeaker = {};
+    
+    // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
+    let speakWord = "Good Bye";
+    
+    // STEP 8: Rewrite the 'speak' function such that it is attached to the
+    // byeSpeaker object instead of being a standalone function.
+    // See Lecture 52, part 2
+    
+    byeSpeaker.speak = function (name) {
+        console.log(speakWord + " " + name);
+    }
+    
+    // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
+    // 'byeSpeaker' on the global scope as well.
+    // xxxx.xxxx = byeSpeaker;
+    
+    window.byeSpeaker = byeSpeaker;
+
+}) (window);
